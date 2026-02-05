f78e5d85234a6736cd53cb626a30b1b4bc883e7a
sonnevaly <valygamer999@gmail.com>
2026-02-05 15:42:31 +0700

    BLA-001 - Rides Service - filter Methods

--- lib/service/rides_service.dart
+++ lib/service/rides_service.dart
@@ -9,20 +9,21 @@ class RidesService {
   //  filter the rides starting from given departure location
   //
   static List<Ride> _filterByDeparture(Location departure) {
-    return [];
+    return availableRides.where((ride) => ride.departureLocation == departure).toList();
   }
 
   //
   //  filter the rides starting for the given requested seat number
   //
   static List<Ride> _filterBySeatRequested(int requestedSeat) {
-    return [];
+    return availableRides.where((ride) => ride.availableSeats == requestedSeat).toList();
   }
 
   //
   //  filter the rides   with several optional criteria (flexible filter options)
   //
   static List<Ride> filterBy({Location? departure, int? seatRequested}) {
-    return [];
+    return availableRides;
   }
 }
+
